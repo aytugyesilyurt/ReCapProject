30f74180610e0661d9dd2168f4751e2809a15026
Aytug <yesilyurt.aytug1@gmail.com>
2021-06-02 00:19:56 +0300

    resultlar yazıldı, business refactor yapılacak

---
+++ Core/Utilities/Results/BaseResults/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results.BaseResults
+{
+    // for basic voids [ void Add() --> IResult Add() ] [Doing methots with message and feedback]
+    public interface IResult
+    {
+        public bool Success { get; }
+        public string Message { get; }
+    }
+}
