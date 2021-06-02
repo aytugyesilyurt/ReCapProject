c06348df30ee1646615fc168bc513fc918fdd63c
Aytug <yesilyurt.aytug1@gmail.com>
2021-06-02 16:42:09 +0300

    user ve customer eklendi tabloları oluşturulacak

---
+++ Entities/Concrete/Persons/Customer.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete.Persons
+{
+    public class Customer:User,IPerson
+    {
+        public int CustomerId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
